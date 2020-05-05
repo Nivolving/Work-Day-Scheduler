a50a239626e26715f06cc6be8bce325803c209ab
Nivolving <nivethapartheeban@gmail.com>
2020-05-05 14:38:48 -0700

    work day scheduler

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,34 @@
-var moment = require('moment');
-moment().format();
+$(document).ready(function(){
+    $('.hour').each(function(){
+       
+        //console.log($(this));
+        var currentHour = moment().startOf('hour');
+        var comparedHour = moment().hour($(this).attr('data-hour')).startOf('hour');
+    
+        var task = localStorage.getItem($(this).attr('data-hour'));
+        $(this).next().children('textarea').val(task);
+
+        if(currentHour > comparedHour){
+            $(this).next().children('textarea').addClass('past');
+        }
+
+        else if(currentHour.isSame(comparedHour,'hour')){
+            $(this).next().children('textarea').addClass('present');
+
+        }
+        else{
+
+            $(this).next().children('textarea').addClass('future');
+
+        }
+    
+    });
+
+    $('.saveBtn').click(function(event) {
+        var task = $(this).prev().children('textarea').val();
+        console.log(task);
+        localStorage.setItem($(this).prev().prev().attr('data-hour'), task);
+
+        
+    });
+});
